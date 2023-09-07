6f8c7cc1602b18d82e90864bf98d8e1d8c3ba19f
dennis githinji <wakahiad@gmail.com>
2023-09-08 02:03:53 +0300

    project setup

---
+++ src/main/java/com/densoft/saccoapi/SaccoApiApplication.java
@@ -0,0 +1,13 @@
+package com.densoft.saccoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaccoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaccoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/densoft/saccoapi/SaccoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.densoft.saccoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaccoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
